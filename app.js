60ec095ef0ac666ba82d9f7783c5a601debc83ea
Tasha1003Natasha <0955712824n@gmail.com>
2022-10-25 20:23:26 +0300

    (ініціалізація express, CORS-прошарок, вилов помилок

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+require("dotenv").config();
 
 const testRouter = require("./routes/api/test");
 
