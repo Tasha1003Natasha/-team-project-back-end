61dd1fd081f2f49083a841290299c4d571309fba
Iryna <pysarenkoiryna@gmail.com>
2022-10-26 18:54:22 +0300

    update

--- controllers/test/getQuestions.js
+++ controllers/test/getQuestions.js
@@ -1,12 +1,12 @@
 const { Test } = require("../../models/test");
 
 const getQuestions = async (req, res) => {
-  const result = await aggregate([
+  const result = await Test.aggregate([
     { $match: { type: "theory" } }, // filter the results
     { $sample: { size: 12 } }, // You want to get 12 docs
   ]);
   console.log(result);
   res.status(201).json(result);
 };
-
+getQuestions();
 module.exports = getQuestions;
--- routes/api/test.js
+++ routes/api/test.js
@@ -5,6 +5,6 @@ const ctrl = require("../../controllers/test");
 const { ctrlWrapper } = require("../../helpers");
 const { schemas } = require("../../models/test");
 
-router.get("/test", ctrlWrapper(ctrl.getQuestions));
+router.get("/", ctrlWrapper(ctrl.getQuestions));
 
 module.exports = router;
