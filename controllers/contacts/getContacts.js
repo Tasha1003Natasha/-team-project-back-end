bd7dc66ee16f8fe2dca15560b3dd1bfdbb40cab1
Tasha1003Natasha <0955712824n@gmail.com>
2022-11-01 16:51:55 +0200

    Коменти

--- controllers/contacts/getContacts.js
+++ controllers/contacts/getContacts.js
@@ -3,9 +3,10 @@ const { Contacts } = require("../../models/contacts");
 const getContacts = async (req, res) => {
   const contacts = await Contacts.find({}, "-createdAt -updatedAt");
   // res.status(201).json(contacts);
-  console.log(contacts);
+  res.json(contacts);
+  // console.log(contacts);
 };
 
-getContacts();
+// getContacts();
 
 module.exports = getContacts;
