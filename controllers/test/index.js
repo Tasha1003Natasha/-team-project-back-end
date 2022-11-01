7f4a2235661545a9df6ec5f9a8f99ca74760bba0
Iryna <pysarenkoiryna@gmail.com>
2022-11-02 01:26:56 +0200

    up

--- controllers/test/index.js
+++ controllers/test/index.js
@@ -1,10 +1,9 @@
-const getTheoryQuestions = require("./getTheoryQuestions");
+
 const getTechQuestions = require("./getTechQuestions");
 const results = require("./results");
 
 
 module.exports = {
-  getTheoryQuestions,
   getTechQuestions,
   results,
 };
