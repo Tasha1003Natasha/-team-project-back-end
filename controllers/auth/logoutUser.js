8da66f3270d64092ed8d02db9d8f6f7913696f37
Tasha1003Natasha <0955712824n@gmail.com>
2022-11-01 20:49:27 +0200

    коммент

--- controllers/auth/logoutUser.js
+++ controllers/auth/logoutUser.js
@@ -4,7 +4,7 @@ const logoutUser = async (req, res) => {
   const { _id } = req.user;
 
   await User.findOneAndUpdate({ _id: _id }, { token: "" }, { new: true });
-  // res.status(204).end()
+  // res.status(204).end();
   res.json({
     messege: "Logout success",
   });
